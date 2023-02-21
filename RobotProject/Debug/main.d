f3b46cbb4eb8f6173117efe3c60979fef8582a75
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-21 15:14:40 +0100

    Set 32 Prescaler

--- RobotProject/Debug/main.d
+++ RobotProject/Debug/main.d
@@ -10,10 +10,11 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- .././IR.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
- .././motor_control.h \
+ .././IR.h .././adc.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ .././motor_control.h .././usart.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
@@ -42,11 +43,17 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 .././IR.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+.././adc.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
 .././motor_control.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+.././usart.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
--- RobotProject/adc.h
+++ RobotProject/adc.h
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
@@ -8,7 +9,7 @@
 void adc_init(){
 	sei(); // Enable global interrupts.
 	ADMUX |= (1<<REFS0); // VREF to AVCC (+5v).
-	ADCSRA |= (1<<ADEN); // Enable ADC and 2 Prescaler.
+	ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0); // Enable ADC and 32 Prescaler.
 }
 
 
@@ -42,11 +43,11 @@ void conversion_init(void){
 }
 
 unsigned int get_adc(uint8_t pin){
-	DIDR0 = (1<<pin); //Disable digital input buffer for the pin.
+	//DIDR0 = (1<<pin); //Disable digital input buffer for the pin.
 	change_channel(pin); // Change channel
 	conversion_init();
 	
-	while(!(ADCSRA & (1<<ADIF)));
+	while((ADCSRA & (1<<ADSC)));
 	return ADC; // 10-bit.
 
 }
