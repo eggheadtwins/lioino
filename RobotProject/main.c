dbd581320d4cc5ef5944123bd8bc217ddc57e5fa
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-21 10:33:54 +0100

    First commit

---
+++ RobotProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * RobotProject.c
+ *
+ * Created: 2023-02-21 10:27:00
+ * Author : SAKA0191
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
