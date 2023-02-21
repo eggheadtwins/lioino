f5af7c295e58878176df8a2e7695897ad9d5d5a1
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-21 14:33:19 +0100

    IR and motor testing

--- RobotProject/main.c
+++ RobotProject/main.c
@@ -1,39 +1,63 @@
 #include <avr/io.h>
 #include "IR.h"
 #include "motor_control.h"
+#include "usart.h"
+#include <util/delay.h>
+
+void test(void);
 
 int main(void)
 {
-	initIRSensors();
-	pwm_timer_init();
 	
+	initIRSensors();
+	pwm_timer_init();	
+	//set_speed(30, 30);
+	/*
+	
+	
+		NOT BELOW 70 WHEN OTHER TURN OFF
+		UNDER 35 FUCK WHEN OTHER IS TURNED ON
+	*/
     /* Replace with your application code */
-    while (1) 
+	
+	
+		
+	
+    while (0) 
     {
 		
-		float track_dir = getTrackDirection();
 		
 		
-		if(track_dir == MIDDLEISH){
-			set_speed(50,70);
-		}
+		usart_send_chars(getTrackDirection());
+    }
+}
+
+
+
+void test(void){
+	float track_dir = getTrackDirection();
 		
 		
-		if(track_dir == -MIDDLEISH){
-			set_speed(70, 50);
+	if(track_dir == MIDDLEISH){
+		set_speed(30,35);
+	}
+		
+		
+	if(track_dir == -MIDDLEISH){
+		set_speed(35, 30);
 			
-		}
+	}
 		
 		
 		if(track_dir == OUTWARD){
-			set_speed(30,70);
+			set_speed(18,35);
 			
 			
 		}
 		
 		
 		if(track_dir == -OUTWARD){
-			set_speed(70,30);
+			set_speed(35,18);
 			
 			
 		}
@@ -41,22 +65,22 @@ int main(void)
 		
 		
 		if(track_dir == BORDER){
-			set_speed(30, 100);
+			set_speed(0, 100);
 			
 			
 		}
 		
 		
 		if(track_dir == -BORDER){
-			set_speed(100, 30);
+			set_speed(100, 0);
 			
 		}
 		
 		if(track_dir == WEIRD_MEASUREMENT){
-			set_speed(50, 50);
+			set_speed(0, 0);
 		}
 		
-		
-    }
+		_delay_ms(3);	
+	
+	
 }
-
