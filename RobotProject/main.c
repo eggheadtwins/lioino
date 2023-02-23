7c6b4131d57fcb9a04bd44a0fd8fc2335a940875
Michel <micheljensen03@gmail.com>
2023-02-23 11:25:33 +0100

    refactored code

--- RobotProject/main.c
+++ RobotProject/main.c
@@ -6,41 +6,21 @@
 #include <util/delay.h>
 
 void test(void);
-unsigned char * tofrray(uint8_t number);
 
-int main(void)
-{
+int main(void) {
 	initIRSensors();
 	pwm_timer_init();	
 	set_speed(30, 30);
 	_delay_ms(20);
 	usart_init();
 
-    while (1) 
-    {
-
+    while (1) {
 		test();
-		
-		
     }
 }
 
 
-
-unsigned char * tofrray(uint8_t number)
-{
-	int n = log10(number) + 1;
-	int i;
-	char *numberArray = calloc(n, sizeof(char));
-	for (i = n-1; i >= 0; --i, number /= 10)
-	{
-		numberArray[i] = (number % 10) + '0';
-	}
-	return numberArray;
-}
-
-
-void test(void){
+void test(void) {
 	float track_dir = getTrackDirection();
 
 	// 1 -> 100, 0
