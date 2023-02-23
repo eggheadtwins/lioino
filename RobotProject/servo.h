f879ef09c462cf83d085fa8efea7880a9135ff32
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-23 17:19:10 +0100

    Create servo.h

---
+++ RobotProject/servo.h
@@ -0,0 +1,28 @@
+
+
+#include <avr/io.h>
+#include <avr/sfr_defs.h>
+
+
+void start_timer(){
+	
+	
+	
+}
+
+
+void servo_pwm_init(void){
+	
+	
+	
+}
+
+
+void set_angle(int angle){
+	
+	
+	
+}
+
+
+
