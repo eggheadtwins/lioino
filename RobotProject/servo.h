65165829a9036867bd0fe500c41f1ad8b5f9a3b1
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-23 17:21:24 +0100

    Add definitions

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -1,7 +1,27 @@
-
-
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+
+#define SERVO_DDRx
+
+#ifdef SERVO_DDRx
+#define SERVO_PIN
+#endif
+
+
+#ifdef SERVO_PIN
+#define SERVO_OCnx
+#endif
+
+#define SERVO_MINIMUM_ANGLE 0
+#define SERVO_MAXIMUM_ANGLE 180
+
+#define PRESCALER 8
+
 
 
 void start_timer(){
