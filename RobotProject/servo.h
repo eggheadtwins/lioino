1ac72ed9a404c5af556e40e8f60a8eafefeb5a42
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-02-27 10:50:04 +0100

    Add Servo control

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -1,46 +1,38 @@
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
+#include <util/delay.h>
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
 
 
-#define SERVO_DDRx
+#define SERVO_DDRx DDRB
 
 #ifdef SERVO_DDRx
-#define SERVO_PIN
-#endif
-
-
-#ifdef SERVO_PIN
-#define SERVO_OCnx
+#define SERVO_PIN PINB2
+#define SERVO_PORT PORTB
 #endif
 
 #define SERVO_MINIMUM_ANGLE 0
 #define SERVO_MAXIMUM_ANGLE 180
 
-#define PRESCALER 8
 
 
-
-void start_timer(){
-	
-	
-	
-}
-
-
-void servo_pwm_init(void){
-	
-	
-	
+void servo_init(void){
+	SERVO_DDRx |= _BV(SERVO_PIN);
 }
 
 
 void set_angle(int angle){
+	angle = (angle > 180)? 180 : angle;
 	
+	int pulseWidth = angle * 11 + 500;
 	
+	SERVO_PORT |= _BV(SERVO_PIN);
+	_delay_ms(pulseWidth);
+	SERVO_PORT &= ~_BV(SERVO_PIN);
+	_delay_ms((20 - pulseWidth) / 1000);	
 	
 }
 
