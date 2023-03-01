2215d182532565044dd0a9c10435c8634a888b7f
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-03-01 14:32:26 +0100

    Update servo.h

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -17,45 +17,42 @@
 
 #define MINIMUM_ANGLE 0
 #define MAXIMUM_ANGLE 180
+#define AFTER_DELAY 5
 
 
 volatile uint8_t previous_angle = 0;
+
 void servo_init(void){
 	SERVO_DDRx |= _BV(SERVO_PIN);
 }
 
-void set(uint8_t angle){
-	SERVO_OCnx = 
-}
-
-
 void set_angle(int angle){
-	int start_angle = 0;
-	int end_angle = 0;
 	if (angle < previous_angle){
-		start_angle = angle;
-		end_angle = MINIMUM_ANGLE;
+		for (int i = angle; i >= MINIMUM_ANGLE; i--){
+			spin(i);
+		}
 	}else if(angle > previous_angle){
-		start_angle = MINIMUM_ANGLE;
-		end_angle = angle;
-		
-	}
-	
-	for (uint8_t i = 0; i <= MAXIMUM_ANGLE; i++)
-	{
-		int pulseWidth = angle * 11 + 500;
-			
-		SERVO_PORT |= _BV(SERVO_PIN);
-		_delay_ms(pulseWidth);
-		SERVO_PORT &= ~_BV(SERVO_PIN);
-		_delay_ms(20);
+		for (int i = previous_angle; i <= angle; i++){
+			spin(i);	
+		}	
 	}
 	
 	previous_angle = angle;
 	
-	
-	
 }
+
+
+void spin(uint8_t angle){
+	int pulse_width = (int) angle * 11 + 500;
+	SERVO_PORT |= _BV(SERVO_PIN);
+	_delay_ms(pulse_width);
+	SERVO_PORT &= ~_BV(SERVO_PIN);
+	_delay_ms(25);
+
+}
+
+
+
 
 
 
