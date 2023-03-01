2c6e8dfba18f76af35c595d1e2febaee5f6bcbcc
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-03-01 10:30:53 +0100

    Add angle control

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -12,27 +12,38 @@
 #ifdef SERVO_DDRx
 #define SERVO_PIN PINB2
 #define SERVO_PORT PORTB
+#define SERVO_OCnx OCR1B
 #endif
 
-#define SERVO_MINIMUM_ANGLE 0
-#define SERVO_MAXIMUM_ANGLE 180
+#define MINIMUM_ANGLE 0
+#define MAXIMUM_ANGLE 180
 
 
 
 void servo_init(void){
 	SERVO_DDRx |= _BV(SERVO_PIN);
+	TCCR1A |= _BV(COM1B1) | _BV(WGM11); // PWM Phase Correct. 
+	ICR1 = 4999; // Top value
+}
+
+void set(uint8_t angle){
+	SERVO_OCnx = 
 }
 
 
 void set_angle(int angle){
-	angle = (angle > SERVO_MINIMUM_ANGLE)? 180 : angle;
+	angle = (angle > MAXIMUM_ANGLE)? 180 : angle;
 	
-	int pulseWidth = angle * 11 + 500;
+	for (uint8_t i = 0; i <= MAXIMUM_ANGLE; i++)
+	{
+		int pulseWidth = angle * 11 + 500;
+			
+		SERVO_PORT |= _BV(SERVO_PIN);
+		_delay_ms(pulseWidth);
+		SERVO_PORT &= ~_BV(SERVO_PIN);
+		_delay_ms(20);
+	}
 	
-	SERVO_PORT |= _BV(SERVO_PIN);
-	_delay_ms(pulseWidth);
-	SERVO_PORT &= ~_BV(SERVO_PIN);
-	_delay_ms((20 - pulseWidth) / 1000);	
 	
 }
 
