510b5aea665795e6d5c183f822d0d5a3bd20b52a
Michel <micheljensen03@gmail.com>
2023-03-03 10:21:42 +0100

    lets hope this works now

--- RobotProject/IR.h
+++ RobotProject/IR.h
@@ -37,9 +37,9 @@ uint16_t getTrackDirection() {
 	uint16_t rightCalibration = right_black - ((left_black + center_black) / 2);
 	right_black -= rightCalibration;
 	
-	if(lapDetection && left_black < 290 && right_black < 290 && center_black < 290) {
+	if(lapDetection && left_black < 300 && right_black < 300 && center_black < 300) {
 		return 1001; // lap detected
-	} else if(left_black > 710 && right_black > 710 && center_black > 710)
+	} else if(left_black > 690 && right_black > 690 && center_black > 690)
 		lapDetection = true;
 		
 	/*
