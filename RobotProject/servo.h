d24b96cdfa1c0d65913163fbefc8bc8aab8882b6
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-03-07 09:41:45 +0100

    Update servo.h

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -1,59 +1,40 @@
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
 
-
 #define SERVO_DDRx DDRB
 
 #ifdef SERVO_DDRx
-#define SERVO_PIN PINB1
-#define SERVO_PORT PORTB
-#define SERVO_OCnx OCR1B
+#	define SERVO_PIN PINB2
+#	define SERVO_PORT PORTB
+#	define SERVO_OCnx OCR1B
 #endif
 
-#define MINIMUM_ANGLE 0
-#define MAXIMUM_ANGLE 180
-#define AFTER_DELAY 5
+#define TOP 19999
+#define SERVO_MAX 40000
+#define SERVO_MIN 8000
 
-
-volatile uint8_t previous_angle = 0;
 
 void servo_init(void){
+	// Set Servo pin as OUTPUT.
 	SERVO_DDRx |= _BV(SERVO_PIN);
 	
+	// Timer 1 - PWM - Mode 14
+	TCCR1A |= _BV(WGM11) | _BV(COM1B1) | _BV(COM1A1);
+	TCCR1B |= _BV(WGM13) | _BV(WGM12);
+	
+	// Set top value
+	ICR1 = TOP;
 }
 
-void ve(void){
-	for (int i = 0; i <= 180; i++)
-	{
-		spin(i);
-	}
-	
-	for (int i = 180; i >= 0; i--)
-	{
-		spin(i);
-	}
-	
-}
 
 void set_angle(int angle){
-	if (angle < previous_angle){
-		for (int i = angle; i >= MINIMUM_ANGLE; i--){
-			spin(i);
-		}
-	}else if(angle > previous_angle){
-		for (int i = previous_angle; i <= angle; i++){
-			spin(i);	
-		}	
-	}
-	
-	previous_angle = angle;
-	
+	SERVO_OCnx = (SERVO_MAX - SERVO_MIN) * angle;
+	TCCR1B |= _BV(CS10);
 }
 
 
