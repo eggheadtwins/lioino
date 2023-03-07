a7becca4c5af6af222c51656557aba9dda2b1602
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-03-07 10:56:19 +0100

    Update servo.h

--- RobotProject/servo.h
+++ RobotProject/servo.h
@@ -9,14 +9,11 @@
 #define SERVO_DDRx DDRB
 
 #ifdef SERVO_DDRx
-#	define SERVO_PIN PINB2
+#	define SERVO_PIN PINB1
 #	define SERVO_PORT PORTB
-#	define SERVO_OCnx OCR1B
 #endif
 
-#define TOP 19999
-#define SERVO_MAX 40000
-#define SERVO_MIN 8000
+#define PRESCALER (_BV(CS11));
 
 
 void servo_init(void){
@@ -24,29 +21,21 @@ void servo_init(void){
 	SERVO_DDRx |= _BV(SERVO_PIN);
 	
 	// Timer 1 - PWM - Mode 14
-	TCCR1A |= _BV(WGM11) | _BV(COM1B1) | _BV(COM1A1);
+	TCCR1A |= _BV(WGM11) | _BV(COM1A1);
 	TCCR1B |= _BV(WGM13) | _BV(WGM12);
+	TCCR1B |= PRESCALER;
+}
+
+
+void set_angle(){
+	for (int i = 0; i <= 4999; i++)
+	{
+		ICR1 = i;
+		_delay_ms(100);
+	}
 	
-	// Set top value
-	ICR1 = TOP;
+	
 }
-
-
-void set_angle(int angle){
-	SERVO_OCnx = (SERVO_MAX - SERVO_MIN) * angle;
-	TCCR1B |= _BV(CS10);
-}
-
-
-void spin(int angle){
-	const int fe = angle * 11 + 500;
-	SERVO_PORT |= _BV(SERVO_PIN);
-	_delay_ms(500);
-	SERVO_PORT &= ~_BV(SERVO_PIN);
-	_delay_ms(25);
-
-}
-
 
 
 
